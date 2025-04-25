66bf0b285e168116844a215139939e5c486cad87
Elisa <eli.randisi@gmail.com>
2025-04-25 15:20:32 +0100

    fix some errors

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,15 +6,15 @@ export const Contact = () => {
     <div className='outer-box'>
       <div className='inner-box'>
         <p className='heading'>LEO HALFEN</p>
-        <p className='text'>leo.halfen@email.com</p>
+        <p><a href="mailto:loewenfilm@gmail.com" className='link'>loewenfilm@gmail.com</a></p>
       </div>
       <div className='inner-box'>
         <p className='heading'>Telephone</p>
-        <p className='text'>00192384848</p>
+        <p>+49 174 3336498</p>
       </div>
       <div className='inner-box'>
         <p className='heading'>FOLLOW</p>
-        <p className='text'>instagram.com/loewe-media</p>
+        <p><a href="https://www.instagram.com/_loewemedia_/" className='link'>instagram.com/_loewemedia_/</a></p>
       </div>
     </div>
   )
