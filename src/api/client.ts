8e2876a149f96e1aeb477c34ee6d9b220556575c
Elisa <elisa@Squeezes-MacBook-Air.local>
2025-12-18 00:35:35 +0000

    fix

---
+++ src/api/client.ts
@@ -0,0 +1,44 @@
+import { createClient } from "contentful";
+
+export const space_id = import.meta.env.CONTENTFUL_SPACE_ID
+export const delivery_token = import.meta.env.CONTENTFUL_DELIVERY_TOKEN
+
+const client = createClient({
+  space: space_id,
+  accessToken: delivery_token
+})
+
+export const fetchPhotos = async (category: string) => {
+  const entryItems = await client.getEntries({
+    content_type: 'photographs',
+    'fields.category': category,
+    'order': 'fields.title',
+  })
+
+  const entries = entryItems.items.map((entry) => {
+    const { fields } = entry
+    return {
+      name: fields.name,
+      imageUrl: fields.imageUrl.fields.file.url,
+    }
+  })
+
+  return { entries }
+}
+
+export const fetchText = async () => {
+  const entryItems = await client.getEntries({
+    content_type: 'descriptions',
+  })
+
+  console.log({entryItems})
+
+  const entries = entryItems.items.map((entry) => {
+    const { fields } = entry
+    return {
+      text: fields.formattedDescription
+    }
+  })
+
+  return { entries }
+}
